30085c23f60672422d0c113fa65f924903873849
Marcel Dumont <marcel@globaltopmedia.com>
2014-06-27 11:01:57 +0200

    add earlier stuff

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+	use google\appengine\api\users\User;
+	use google\appengine\api\users\UserService;
+
+    # Looks for current Google account session
+    $user = UserService::getCurrentUser();
+    if ($user) {
+    	echo 'Hello, ' . htmlspecialchars($user->getNickname());
+    }
+    else {
+    	header('Location: ' . UserService::createLoginURL($_SERVER['REQUEST_URI']));
+    }
+
+?>
+<html>
+    <head>
+    <link type="text/css" rel="stylesheet" href="/stylesheets/main.css" />
+    </head>
+    
+    </html>
